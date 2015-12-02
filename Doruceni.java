fc726c40a6f2e4b1bb09bdfdd1dddda4b330d593
Michal <Michal@147.228.128.82>
2015-12-02 12:10:22 +0100

    javaDoc doruceni

--- Doruceni.java
+++ Doruceni.java
@@ -1,39 +1,62 @@
 package pt;
 
+/*******************************************************
+ * Instance třídy {@code Doruceni} představjí
+ * přepravku pro uchování dat o doruceni.
+ * 
+ * @author Michal Štrunc a Jakub Váverka
+ *
+ */
 public class Doruceni {
-	private int kolik;
-	private Lod lod;
-	private int den;
 	
+	/** množství doručených léků*/
+	private final int kolik;
+	
+	/** lod, která doručila objednávku*/
+	private final Lod lod;
+	
+	/** den doručení*/
+	private final int den;
+	
+	/*************************************************
+	 * vytvoří instanci zadaného množštví léků, 
+	 * doručovací lodí a dnem doručení.
+	 * 
+	 * @param kolik množství doručených léků
+	 * @param l lod přepravující náklad
+	 * @param den den doručení
+	 */
 	public Doruceni(int kolik, Lod l, int den){
 		this.kolik = kolik;
 		this.lod = l;
 		this.den = den;
 	}
 
+	/***********************************************
+	 * vrátí množství doručených léků
+	 * 
+	 * @return množství léků
+	 */
 	public int getKolik() {
 		return kolik;
 	}
 
-	public void setKolik(int kolik) {
-		this.kolik = kolik;
-	}
-
+	/***********************************************
+	 * vrátí instanci třídy {@code Lod},  která
+	 * představuje doručující lodl
+	 *
+	 * @return lod, která doručila náklad
+	 */
 	public Lod getLod() {
 		return lod;
 	}
 
-	public void setLod(Lod lod) {
-		this.lod = lod;
-	}
-
+	/**********************************************
+	 * vrátí den doručení léků
+	 * 
+	 * @return den doručení
+	 */
 	public int getDen() {
 		return den;
-	}
-
-	public void setDen(int den) {
-		this.den = den;
-	}
-	
-	
+	}	
 }
