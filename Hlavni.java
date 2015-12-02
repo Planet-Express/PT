f0bfe9f6c006189cb781b14ee22efacea3a9a686
Michal <Michal@147.228.128.82>
2015-12-02 18:42:38 +0100

    javaDoc hlavni

--- Hlavni.java
+++ Hlavni.java
@@ -4,12 +4,28 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+/**************************************************
+ * Instance třídy {@code Hlavni} představuje
+ * hlvní třídu aplikace, která ma nastorosti 
+ * chod aplikace.
+ * 
+ * @author Michal Štrunc a Jakub Váverka
+ *
+ */
 public class Hlavni extends Application{
 	
-	static Galaxie g;
+	/** galaxie*/
+	private static Galaxie g;
 	
-	static GUI gui = GUI.getINSTANCE();
+	/** gui*/
+	private static GUI gui = GUI.getINSTANCE();
 	
+	/*************************************************
+	 * je hlavní metodou aplikace, která
+	 * řídí chod aplikace.
+	 * 
+	 * @param args argumenty příkazové řádky
+	 */
 	@SuppressWarnings("deprecation")
 	public static void main(String[] args){
 		//g = new Galaxie(800,5000);
@@ -20,6 +36,9 @@ public class Hlavni extends Application{
 		gui.getCas().stop();
 	}
 
+	/************************************************
+	 * spustí gui aplikace
+	 */
 	@Override
 	public void start(Stage primaryStage){
 		Scene s = new Scene(gui.createScene(g));
